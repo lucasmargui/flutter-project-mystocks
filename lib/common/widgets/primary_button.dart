79ceea76e75fc7a514945064760fe296c415f44c
Lucas Martins Guimarães <lucasmargui@outlook.com>
2024-02-23 14:21:08 -0300

    Refatoração de primary_button

--- lib/common/widgets/primary_button.dart
+++ lib/common/widgets/primary_button.dart
@@ -1,48 +1,39 @@
 // ignore_for_file: unnecessary_null_comparison, prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
-
 import '../constants/app_colors.dart';
 import '../constants/app_text_style.dart';
 
 class PrimaryButton extends StatelessWidget {
-  // VoidCallback é a mesma coisa que uma void Function, ou seja uma função que não tem retorno e a exclamação no final significa que ela pode ser nula
-  // final VoidCallBack? = final void Function()? ;
-
   final VoidCallback? onPressed;
   final String text;
-  const PrimaryButton({Key? key, this.onPressed, required this.text})
+
+  PrimaryButton({Key? key, this.onPressed, required this.text})
       : super(key: key);
+
+  BorderRadius _borderRadius = BorderRadius.circular(24.0);
 
   @override
   Widget build(BuildContext context) {
-    return Material(
+    return InkWell(
+      borderRadius: _borderRadius,
+      onTap: onPressed,
       child: Ink(
+        height: 48,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(38.0),
+          borderRadius: _borderRadius,
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            end: Alignment.topLeft,
+            stops: [0.1, 0.9],
+            colors: onPressed != null
+                ? AppColors.blueDarkGradient
+                : AppColors.greyDarkGradient,
+          ),
         ),
-        child: InkWell(
-          borderRadius: BorderRadius.circular(38.0),
-          onTap: onPressed,
-          child: Container(
-            alignment: Alignment.center,
-            height: 64.0,
-            width: 300.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(38.0),
-              gradient: LinearGradient(
-                begin: Alignment.bottomRight,
-                end: Alignment.topLeft,
-                stops: [0.1, 0.9],
-                colors: onPressed != null
-                    ? AppColors.blueDarkGradient
-                    : AppColors.greyDarkGradient,
-              ),
-            ),
-            child: Text(
-              text,
-              style:
-                  AppTextStyles.mediumText18.copyWith(color: AppColors.white),
-            ),
+        child: Align(
+          child: Text(
+            text,
+            style: AppTextStyles.mediumText18.copyWith(color: AppColors.white),
           ),
         ),
       ),
