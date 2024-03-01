d30b3e936c296bccd227c32f5db28846e3ab90cc
Lucas Martins Guimarães <lucasmargui@outlook.com>
2024-02-29 22:59:10 -0300

    update: index functions default

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,10 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const request = require("graphql-request");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const client = new request.GraphQLClient('', {
+    headers: {
+        "content-type": "application/json",
+        "x-hasura-admin-secret": "" 
+    }
+})
